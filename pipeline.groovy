17a509359f8678415da37fb51bc963b9cd61ea73
akshay19 <98883372+akshayparsade@users.noreply.github.com>
2025-03-21 14:51:54 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -14,9 +14,9 @@ pipeline{
         stage("Deploy"){
            steps{
                 sh '''
-                   docker build . -t my_cbz_project:latest
-                   docker push my_cbz_project:latest
-                   docker rmi my_cbz_project:latest
+                   docker build . -t akshayparsade/my_cbz_project:latest
+                   docker push akshayparsade/my_cbz_project:latest
+                   docker rmi akshayparsade/my_cbz_project:latest
                    kubectl apply -f ./deploy/
                  '''
             }
